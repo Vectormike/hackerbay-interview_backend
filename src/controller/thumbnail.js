c24ef95dc4789e8ae66b3b7ac81de0dc2fa813c9
Redeemer40 <victorjonah199@gmail.com>
2020-03-26 23:31:07 +0100

    contr: Thumbnail controller

---
+++ src/controller/thumbnail.js
@@ -0,0 +1,25 @@
+import Jimp from "jimp";
+
+/**
+ * Image Thumbnail Generation
+ */
+class CreateThumbnail {
+  static async create(req, res) {
+    try {
+      const image = await Jimp.read(req.body.image);
+      if (error) throw err;
+      image.resize(50, 50).write("thumbnail.jpg");
+      res.json({
+        success: true,
+        message: "Image generated"
+      });
+    } catch (error) {
+      res.json({
+        success: false,
+        message: "Image generation failure"
+      });
+    }
+  }
+}
+
+export default CreateThumbnail;
