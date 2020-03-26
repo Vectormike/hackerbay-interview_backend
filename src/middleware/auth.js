3575e5b9165cb481b0e32c8700e5edb38c85fae0
Redeemer40 <victorjonah199@gmail.com>
2020-03-26 22:23:41 +0100

    func: jsonwebtokens

---
+++ src/middleware/auth.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const secret = process.env.SECRET;
+
+class AuthMiddleware {
+  static async verifyToken(token) {
+    try {
+      jwt.verify(token, secret, { expiresIn: "24hr" }, (error, decoded) => {
+        if (error) {
+          res.status(401).json({ msg: "Token is not valid" });
+        } else {
+          req.user = decoded.user;
+          next();
+        }
+      });
+    } catch (err) {
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+
+  static async generateToken(newToken) {
+    try {
+      jwt.sign(newToken, secret, { expiresIn: "24hr" }, (error, token) => {
+        if (error) {
+          throw error;
+        }
+        return token;
+      });
+    } catch (error) {
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+}
+
+export default AuthMiddleware;
